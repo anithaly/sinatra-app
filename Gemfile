5f940af8acb8da4a07b764b11945c76fb8f59075
Natalia Stanko <anithaly@gmail.com>
2014-02-26 21:31:51 +0100

    crud

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://rubygems.org'
+
+gem 'data_mapper'
+gem 'haml'
+gem 'sinatra'
+gem 'pry'
+gem 'sinatra-contrib'
+gem 'dm-sqlite-adapter'
+gem 'sinatra-partial'
+gem 'dm-migrations'
+gem 'dm-core'
--- myapp.rb
+++ myapp.rb
@@ -1,44 +1,124 @@
-require 'sinatra'
+require "bundler/setup"
+require 'rubygems'
+require "sinatra"
+require "sinatra/partial"
+require 'sinatra/contrib'
+require "sinatra/reloader" if development?
+require 'data_mapper' # metagem, requires common plugins too.
+require 'dm-core'
+require 'dm-migrations'
+require 'pry'
+
+SITE_TITLE = "Sinatra app"
+
+enable :sessions
+
+# need install dm-sqlite-adapter
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+
+class Article
+    include DataMapper::Resource
+    property :id, Serial
+    # property :user_id, Integer
+    # belongs_to :post
+    # belongs_to :post, :parent_key => [:id], :child_key => [:post_id]
+    # property   :post_id, Integer
+    property :title, String
+    property :body, Text
+    property :created_at, DateTime
+end
+
+class User
+    include DataMapper::Resource
+    property :id, Serial
+    property :name, String
+    property :email, String
+    property :created_at, DateTime
+end
+
+DataMapper.auto_upgrade!
+
+# # Perform basic sanity checks and initialize all relationships
+# # Call this when you've defined all your models
+# DataMapper.finalize
+
+# # automatically create the tables
+# Article.auto_upgrade!
+# User.auto_upgrade!
 
 set :haml, :format => :html5, :layout_engine => :haml, :layout => :layout
 
 get '/' do
   @title = 'Welcome!'
-  haml :home, :locals => {:title => @title}
-end
-
-get '/hello/:name' do
-  # matches "GET /hello/foo" and "GET /hello/bar"
-  # params[:name] is 'foo' or 'bar'
-  @title = @hello = "Hello #{params[:name]}!"
-  haml :hello, :locals => {:title => @title, :hello => @hello}
+  haml :index, :locals => {:title => @title}
 end
 
 get '/about' do
   @title = 'About'
-  haml :about, :locals => {:title => @title}
+  haml :about
 end
+
+#articles
 
 get '/articles' do
   @title = 'List of articles'
-  haml :index, :locals => {:title => @title}
+  @articles = Article.all(:order => [ :id.desc ], :limit => 20)
+  haml :list
 end
 
-get '/articles/create' do
-  etag '', :new_resource => true
-  Article.create
-  erb :new_article
+get '/articles/new' do
+  haml :new, :locals => {
+    :article => Article.new,
+    :action => '/articles/create'
+  }
 end
 
-get '/article/:id' do
-  @article = Article.find params[:id]
-  last_modified @article.updated_at
-  etag @article.sha1
-  erb :article
+post '/articles/create' do
+  article = Article.create(:title => params[:title], :body => params[:body])
+  article = Article.new
+  article.attributes = params['article']
+  article.save
+  redirect "/articles/#{article.id}"
 end
+
+get '/articles/:id' do
+  # @article = Article.find params[:id]
+  @article = Article.get params[:id]
+  # last_modified @article.updated_at
+  # etag @article.sha1
+  # c = article.get(id)
+   haml :show
+end
+
+# Show form to edit article
+ get '/articles/:id/edit' do|id|
+   article = Article.get(id)
+   haml :edit, :locals => {
+    :article => article,
+    :action => "/articles/#{article.id}/update"
+   }
+ end
+
+ # Edit a article
+ post '/articles/:id/update' do|id|
+   article = Article.get(id)
+   article.update params['article']
+
+   redirect "/articles/#{id}"
+ end
+
+ # Delete a article
+ post '/articles/:id/destroy' do|id|
+   article = Article.get(id)
+   article.destroy
+
+   redirect "/articles"
+ end
+
+  not_found do
+    halt 404, 'page not found'
+  end
 
 =begin
-delete '/' do
-  #.. annihilate something ..
-end
+  comments
 =end
